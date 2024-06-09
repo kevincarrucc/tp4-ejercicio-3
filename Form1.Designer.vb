a8df5ac44b95f7f9be1baede369900060a6d483d
carru <carrucciokevin@gmail.com>
2024-06-09 18:45:35 -0300

    Generacion de codigo y detalles esteticos

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,13 +25,16 @@ Partial Class Form1
         Me.Boton = New System.Windows.Forms.Button()
         Me.GrillaMatrizDiagonales = New System.Windows.Forms.DataGridView()
         Me.GrillaMatrizEntera = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         CType(Me.GrillaMatrizDiagonales, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GrillaMatrizEntera, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Boton
         '
-        Me.Boton.Location = New System.Drawing.Point(363, 183)
+        Me.Boton.Location = New System.Drawing.Point(50, 361)
         Me.Boton.Name = "Boton"
         Me.Boton.Size = New System.Drawing.Size(75, 23)
         Me.Boton.TabIndex = 0
@@ -45,24 +48,54 @@ Partial Class Form1
         Me.GrillaMatrizDiagonales.Location = New System.Drawing.Point(444, 12)
         Me.GrillaMatrizDiagonales.Name = "GrillaMatrizDiagonales"
         Me.GrillaMatrizDiagonales.RowHeadersVisible = False
-        Me.GrillaMatrizDiagonales.Size = New System.Drawing.Size(246, 194)
+        Me.GrillaMatrizDiagonales.Size = New System.Drawing.Size(344, 194)
         Me.GrillaMatrizDiagonales.TabIndex = 3
         '
         'GrillaMatrizEntera
         '
         Me.GrillaMatrizEntera.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.GrillaMatrizEntera.ColumnHeadersVisible = False
-        Me.GrillaMatrizEntera.Location = New System.Drawing.Point(111, 12)
+        Me.GrillaMatrizEntera.Location = New System.Drawing.Point(12, 12)
         Me.GrillaMatrizEntera.Name = "GrillaMatrizEntera"
         Me.GrillaMatrizEntera.RowHeadersVisible = False
-        Me.GrillaMatrizEntera.Size = New System.Drawing.Size(246, 194)
+        Me.GrillaMatrizEntera.Size = New System.Drawing.Size(345, 194)
         Me.GrillaMatrizEntera.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 209)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Grilla Matriz"
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Location = New System.Drawing.Point(8, 428)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(161, 13)
+        Me.lblCount.TabIndex = 6
+        Me.lblCount.Text = "Cantidad de numeros ingresados"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(441, 209)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Grilla diagonales"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.GrillaMatrizEntera)
         Me.Controls.Add(Me.GrillaMatrizDiagonales)
         Me.Controls.Add(Me.Boton)
@@ -71,10 +104,14 @@ Partial Class Form1
         CType(Me.GrillaMatrizDiagonales, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GrillaMatrizEntera, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Boton As Button
     Friend WithEvents GrillaMatrizDiagonales As DataGridView
     Friend WithEvents GrillaMatrizEntera As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblCount As Label
+    Friend WithEvents Label3 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -6,29 +6,22 @@
         GrillaMatrizDiagonales.Rows.Add(5)
     End Sub
     Private Sub Boton_Click(sender As Object, e As EventArgs) Handles Boton.Click
-        Dim matriz(5, 5) As Integer
+        Dim matriz(5, 5), count As Integer
+        count = 0
         For f = 0 To 4
             For c = 0 To 4
-
+                matriz(f, c) = InputBox("ingresar 25 valores")
+                count = count + 1
+                lblCount.Text = count & "/25 numeros ingresados"
+                GrillaMatrizEntera.Item(c, f).Value = matriz(f, c)
+                If f = c Then
+                    GrillaMatrizDiagonales.Item(f, c).Value = matriz(f, c)
+                End If
             Next
         Next
-
-        For f = 0 To 4
-            For c = 0 To 4
-
-            Next
-        Next
-
-        For f = 0 To 4
-            For c = 0 To 4
-
-            Next
-        Next
-
-        For f = 0 To 4
-            For c = 0 To 4
-
-            Next
-        Next
+        GrillaMatrizDiagonales.Item(4, 0).Value = matriz(0, 4)
+        GrillaMatrizDiagonales.Item(3, 1).Value = matriz(1, 3)
+        GrillaMatrizDiagonales.Item(1, 3).Value = matriz(3, 1)
+        GrillaMatrizDiagonales.Item(0, 4).Value = matriz(4, 0)
     End Sub
 End Class
